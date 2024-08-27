01b9bedd405011206a07d676a338ce4ba9b94c3f
Daniele Picone <danielepicone97@gmail.com>
2024-08-27 12:21:23 +0200

    cicli2

---
+++ cicli2.js
@@ -0,0 +1,33 @@
+let contatore = 0;
+let somma = 0;
+
+for(let i = 0 ; i <= 20; i ++){
+
+   if ( i % 2 !== 0 ){
+    somma  += i;
+    contatore += 1;
+   } else {
+    pari = i
+   }
+    
+   media = somma / contatore 
+
+
+console.log(pari);
+
+}
+
+console.log(somma);
+console.log(contatore);
+console.log(media);
+
+   
+    
+    
+  
+ 
+    
+
+
+
+
