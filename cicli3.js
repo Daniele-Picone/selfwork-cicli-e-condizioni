d48cf130ce33f4843350b2d4878634b2cc9c7cf9
Daniele Picone <danielepicone97@gmail.com>
2024-08-31 13:08:01 +0200

    correzione-cicli3

--- cicli3.js
+++ cicli3.js
@@ -1,28 +1,54 @@
-let num = prompt('scegli la tua bevanda')
+// let num = prompt('scegli la tua bevanda')
 
 
 
-while ( num  ) {
-    num = prompt('scegli la tua bevanda')
+// while ( num  ) {
+//     num = prompt('scegli la tua bevanda')
     
-    if( num == 1){
+//     if( num == 1){
+//         console.log(`hai scelto l'acqua`);
+        
+//     }else if (num == 2){
+//         console.log(`hai scelto la coca cola`);
+         
+//     }else if ( num == 3) {
+//         console.log(`hai scelto la birra`);
+        
+//     } else if ( num >= 3 || num <=0 ){
+//         console.log('scelta non valida');
+        
+//     } else if ( num !== Number){
+//         console.log('scelta non valida');
+
+//     }
+// }
+
+
+// Correzione
+
+let scelta;
+
+do{
+   scelta = Number(prompt('scegli la tua bevanda- 1 acuqa - 2 coca cola - 3 birra'));
+   
+
+   switch (scelta) {
+    case  1:
         console.log(`hai scelto l'acqua`);
         
-    }else if (num == 2){
-        console.log(`hai scelto la coca cola`);
-         
-    }else if ( num == 3) {
-        console.log(`hai scelto la birra`);
-        
-    } else if ( num >= 3 || num <=0 ){
-        console.log('scelta non valida');
-        
-    } else if ( num !== Number){
+        break;
+        case 2:
+            console.log(`hai scelto la coca cola`);
+          break;
+        case 3 :
+            console.log('hai scelto la birra3'); 
+        break;
+    default:
         console.log('scelta non valida');
 
-    }
-}
+        break;
+   }
+}while( scelta <1 || scelta > 3 );
 
-
-
+                                                             
 
